eeb2bca7b89d7e6c98839949fef7e22d8746851f
Pedro Bonfilio <pedroblimaa@gmail.com>
2024-09-29 11:13:21 -0300

    Add select cloud folder FE side

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,13 +1,31 @@
+import { open } from '@tauri-apps/api/dialog'
+import { SyntheticEvent, useState } from 'react'
+import { FaFolder } from 'react-icons/fa'
 import './Settings.css'
 
 function Settings() {
+  const [folder, setFolder] = useState('')
+
+  const selectFolder = async () => {
+    const selected = await open({ directory: true, multiple: false })
+    setFolder(selected as string)
+  }
+
+  const changeFolderValue = (event: SyntheticEvent<HTMLInputElement>) => {
+    console.log(event);
+    setFolder(event.currentTarget.value);
+  };
+
   return (
-    <div>
-      <div>
-        Cloud folder
-        <input type="file" />
+    <>
+      <div className="setting-container">
+        <div className="setting-name">Cloud Folder:</div>
+        <button className="setting-button" onClick={selectFolder}>
+          <FaFolder className='setting-icon'/>
+        </button>
+        <input name="Cloud Folder" id="cloud_folder" value={folder} onChange={changeFolderValue} />
       </div>
-    </div>
+    </>
   )
 }
 
