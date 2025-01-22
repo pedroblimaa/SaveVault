1ae78f45569bbea3622a9b142d7605f12a9b2ffe
Pedro Bonfilio <pedroblimaa@gmail.com>
2025-01-21 21:01:12 -0300

    Upgrade tauri version and fix broken stuff

--- src/services/SettingsService/SettingsService.ts
+++ src/services/SettingsService/SettingsService.ts
@@ -1,15 +1,16 @@
-import { invoke, dialog } from '@tauri-apps/api'
+import { invoke } from '@tauri-apps/api/core'
 import { texts } from '../../utils/config'
+import { open } from '@tauri-apps/plugin-dialog'
 
 export class SettingsService {
 
   static async handleSetFolder(folder: string): Promise<string> {
     if (folder?.length) {
-      const confirmMove = await confirm(texts.confirmMoveFolder())
+      const confirmMove = await window.confirm(texts.confirmMoveFolder())
       if (!confirmMove) return folder
     }
 
-    const selected = await dialog.open({ directory: true, multiple: false })
+    const selected = await open({ directory: true, multiple: false })
     const isFolderAlreadyUsed = await invoke('check_is_folder_already_used', { path: selected })
 
     const override = isFolderAlreadyUsed ? await confirm(texts.confirmOverrideFolder()) : true
