fdd19ef7a0caecb9189805b4004b667de44fd6d8
Pedro Bonfilio <pedroblimaa@gmail.com>
2025-01-21 21:04:09 -0300

    Fix a few tauri tests involving window

--- src/services/GameService/GameService.test.ts
+++ src/services/GameService/GameService.test.ts
@@ -1,21 +1,23 @@
-import { invoke } from "@tauri-apps/api"
-import { expect, Mock, test } from "vitest"
-import { GameService } from "./gameService"
+import { invoke } from "@tauri-apps/api/core"
+import { describe, expect, Mock, test } from "vitest"
+import { GameService } from "./GameService"
 import { mockGames } from "./GameService.data.mock"
 
 
-test("should fetch games and add loading equals to false", async () => {
-  // Given
-  (invoke as Mock).mockResolvedValue(mockGames)
-  const expectedGames = [
-    { ...mockGames[0], loading: false },
-    { ...mockGames[1], loading: false },
-  ]
+describe('fetchGames', () => {
+  test("should fetch games and add loading equals to false", async () => {
+    // Given
+    (invoke as Mock).mockResolvedValue(mockGames)
+    const expectedGames = [
+      { ...mockGames[0], loading: false },
+      { ...mockGames[1], loading: false },
+    ]
 
-  // When
-  const games = await GameService.fetchGames()
+    // When
+    const games = await GameService.fetchGames()
 
-  // Then
-  expect(invoke).toHaveBeenCalledWith('get_games')
-  expect(games).toEqual(expectedGames)
+    // Then
+    expect(invoke).toHaveBeenCalledWith('get_games')
+    expect(games).toEqual(expectedGames)
+  })
 })
--- src/services/SettingsService/SettingsService.test.ts
+++ src/services/SettingsService/SettingsService.test.ts
@@ -1,23 +1,61 @@
-import { dialog, invoke } from "@tauri-apps/api"
-import { expect, test, vi } from "vitest"
-import { SettingsService } from "./settingsService"
+import { open } from "@tauri-apps/plugin-dialog"
+import { invoke } from "@tauri-apps/api/core"
+import { describe, expect, Mock, test, vi } from "vitest"
+import { SettingsService } from "./SettingsService"
+import { texts } from "../../utils/config"
 
+// Mock the @tauri-apps/api module
+vi.mock("@tauri-apps/api/core", () => ({
+  invoke: vi.fn(),
+}))
 
+// Mock the @tauri-apps/plugin-dialog module
+vi.mock("@tauri-apps/plugin-dialog", () => ({
+  open: vi.fn(),
+}))
 
-test("should fetch games and add loading equals to false", async () => {
-  // Given
-  const openSpy = vi.spyOn(dialog, 'open').mockResolvedValue('C:\\')
-  vi.mocked(invoke).mockImplementation((cmd) => {
-    if (cmd === 'check_is_folder_already_used') {
-      return Promise.resolve(false)
-    }
-    return Promise.resolve(undefined)
+vi.stubGlobal('confirm', () => Promise.resolve(true))
+
+describe("handleSetFolder", () => {
+  test("should set folder when called with no value", async () => {
+    // Given
+    (open as Mock).mockResolvedValue('C:\\')
+    const confirmSpy = vi.spyOn(globalThis, 'confirm')
+    confirmSpy.mockResolvedValue(true);
+    (invoke as Mock).mockImplementation((cmd: string) => {
+      if (cmd === 'check_is_folder_already_used') {
+        return Promise.resolve(false)
+      }
+      return Promise.resolve(undefined)
+    })
+
+    // When
+    await SettingsService.handleSetFolder('')
+
+    // Then
+    expect(confirmSpy).not.toHaveBeenCalled()
+    expect(open).toHaveBeenCalledWith({ directory: true, multiple: false })
+    expect(invoke).toHaveBeenCalledWith('set_cloud_location', { path: 'C:\\', overrideFolder: true })
   })
 
-  // When
-  await SettingsService.handleSetFolder('')
+  test("should set folder and confirm when called with some value", async () => {
+    // Given
+    (open as Mock).mockResolvedValue('C:\\')
+    const confirmSpy = vi.spyOn(globalThis, 'confirm')
+    confirmSpy.mockResolvedValue(true)
+    vi.mocked(invoke).mockImplementation((cmd) => {
+      if (cmd === 'check_is_folder_already_used') {
+        return Promise.resolve(false)
+      }
+      return Promise.resolve(undefined)
+    })
 
-  // Then
-  expect(openSpy).toHaveBeenCalledWith({ directory: true, multiple: false })
-  expect(invoke).toHaveBeenCalledWith('set_cloud_location', { path: 'C:\\', overrideFolder: true })
+    // When
+    await SettingsService.handleSetFolder('C:\\someFolder')
+
+    // Then
+    expect(confirmSpy).toHaveBeenCalledWith(texts.confirmMoveFolder())
+    expect(open).toHaveBeenCalledWith({ directory: true, multiple: false })
+    expect(invoke).toHaveBeenCalledWith('set_cloud_location', { path: 'C:\\', overrideFolder: true })
+  })
 })
